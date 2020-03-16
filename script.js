9c4a76fc8a7c20345a2afb37826eaeccd8a72fd2
Julien Rousseau <julienrousseau16@gmail.com>
2020-03-16 10:45:50 +0100

    first submission of solution for quest 1

---
+++ script.js
@@ -0,0 +1,5 @@
+const favouriteMovie = "Drive"
+const yearOfRelease = 2011
+const director = "Nicolas Winding Refn"
+message = "Un de mes films coup de coeur, c'est " + favouriteMovie + ", réalisé par " + director + " et sorti à l'automne " + yearOfRelease
+alert(message)
